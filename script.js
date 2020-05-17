300d34229dc2dcaceb13281378b96b0c6c2f3014
CARL MARTIN <cj.martin777@gmail.com>
2020-05-16 23:37:35 -0500

    Quiz page

---
+++ script.js
@@ -0,0 +1,26 @@
+
+
+
+prompt("would you like to take this test?", "name");
+    if ((person !=null)(startTimer('timer')));
+
+var counter1 =0;
+console.log++;
+var counter2 =0;
+ console.log++;
+
+ 
+ function rightAnswer() {
+    alert ("right!");
+    (counter1.rightAnswer, 1++); 
+    return(console.log++);
+};
+
+
+function wrongAnswer() {
+    alert("nope!");
+    wrongAnswer = timer -60000;
+    counter2.wrongAnswer, 1++;
+    return(console.log++);
+ };
+
