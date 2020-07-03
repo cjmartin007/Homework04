ba1a5fd02cb4d3c7a77f6d851b56ae22c3f2158e
CARL MARTIN <cj.martin777@gmail.com>
2020-07-03 10:16:42 -0500

    modified start button, added functionality to start button

---
+++ assets/script.js
@@ -0,0 +1,22 @@
+function Start() {
+    const app = document.getElementById("container1");
+    if (app.style.display === "none") {
+        app.style.display = "block";
+    } else {
+        app.style.display = "none";
+    }
+    if (app.style.display === "block") {
+    const st = document.getElementById("Start");
+        st.style.display = "none";
+    }
+//    
+//    if (st.style.display = "none") {
+// const name = prompt("would you like to take this test?", "name");
+//  if (name != "") {
+//      document.getElementById("info").innerHTML = 
+//    "name:" + name;
+//    console.log(name);
+//  }};
+};
+  
+
