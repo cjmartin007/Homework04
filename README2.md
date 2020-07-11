94ac8f25fd706eb4d4e43d8cb0eb24b2201008f0
CARL MARTIN <cj.martin777@gmail.com>
2020-07-11 01:49:21 -0500

    I finally created a proper README.MD in README2.md fabricated some proper questions via my Modals. As well as had them function accordingly

--- README2.md
+++ README2.md
@@ -1,3 +1,42 @@
 ***README***
 
+This test was made as a test of knowledge of JS
+
+WHEN you click the start button.
+THEN the timer starts.
+WHEN you click the start button.
+THEN the first question pops up.
+WHEN you click the CORRECT button.
+THEN time is added to your timer.
+WHEN you click the CORRECT button.
+THEN you will get a score in the right answer score board.
+WHEN you click the wrong button.
+THEN you will get a score in the wrong answer score board.
+WHEN you click the wrong button.
+THEN time is deducted from your timer.
+WHEN you finish your test or run out of time.
+THEN the test is over and your information and score will be logged.
+
+********
+
+***FUNCTIONS***
+Allot of the functions were repettative. I could not figure out how to make them all function as one entity. but as soon as i figured out how to make them work i ran it. testing as i went to make sure every little bit worked though allot is just repetitive. i did what i could with the knowledge i have. 
+******
+
+***MODALS?***
+Because i couldn't figure out how to make them run as a single entity i made them all each their own individual entity and used "block" and "none" to pull and push them from the on page view.
+
+***PAGE INFO***
+I couldnt quite figure out how to reset my variables back to their original numbers time =(180) aka 3 minutes and my counterR(Right) and counterW(Wrong) back to "0" the name seems to rest just fine.
+
+***Q/A***
+I spent roughly 100hrs trying to research different ways to do this homework (I get it I'm not that good at Google yet).
+
+The questions I came up with myself(obviously). From knowledge that I gained over the course of this class to the best of my knowledge.
+
+Allot of coffee went into the creation of this project.
+
+I CAN actually explain everything going on in the code and why it works. For... YOU people.   
+
+I'm not sure what I was doing wrong but I couldnt get the log info to append to a board the way I wanted it to. (I dont think I was dong things in the right order). Try as I might I couldn't get that to function properly. (Keep in mind this class is my first any computer anything). So don't be a phallic related object. If you actually care to make me better help a guy out and correct me instead of being a canis lupus familiaris. 
 
--- assets/script.js
+++ assets/script.js
@@ -12,6 +12,9 @@ let q10 = document.getElementById("container10");
 let time = 180;
 let timeLeft;
 
+let counterC = 0;
+let counterW = 0;
+
 function Start() {
     const q1 = document.getElementById("container1"); //1
     const st = document.getElementById("Start"); //2
@@ -48,17 +51,15 @@ function timer() {
    //console.log( time--);
   if(time < 0) {
     clearInterval(timeLeft);
-    console.log("Times Up!" , name , score);
+    console.log("Times Up!" , info , score);
     document.getElementById("timer").innerHTML = 
     "TIMES UP!";
   }
 };
 
- let counterC = 0;
-let counterW = 0;
-//let score1 = "Right:" + counterC + " " + "Wrong:" + counterW;
-
+//question 1
   function rightAnswer() {
+
     let q1 = document.getElementById("container1");
     let q2 = document.getElementById("container2");
 
@@ -78,33 +79,8 @@ let counterW = 0;
 
     console.log("right!" + " " + counterC); 
 
+  } function wrongAnswer() {
 
-  }
-  function rightAnswer2() {
-    let q2 = document.getElementById("container2");
-    let q3 = document.getElementById("container3");
-
-    if (q2.style.display === "block") {
-        q2.style.display = "none"      
-    } 
-    if (q3.style.display === "none") {
-        q3.style.display = "block"
-    }
-    counterC++;
-
-    time++;time++;time++;time++;time++;
-    time++;time++;time++;time++;time++;
-   
-    const scr = document.getElementById("score").innerHTML =
-    "Right:" + counterC + "Wrong:" + counterW;
-
-    console.log("right!" + " " + counterC); 
-
-
-  }
-
-  
-  function wrongAnswer() {
     let q1 = document.getElementById("container1");
     let q2 = document.getElementById("container2");
     
@@ -128,7 +104,33 @@ let counterW = 0;
 
     console.log("wrong" + " " + counterW); 
   };
-  function wrongAnswer2() {
+
+//question 2
+
+  function rightAnswer2() {
+
+    let q2 = document.getElementById("container2");
+    let q3 = document.getElementById("container3");
+
+    if (q2.style.display === "block") {
+        q2.style.display = "none"      
+    } 
+    if (q3.style.display === "none") {
+        q3.style.display = "block"
+    }
+    counterC++;
+
+    time++;time++;time++;time++;time++;
+    time++;time++;time++;time++;time++;
+   
+    const scr = document.getElementById("score").innerHTML =
+    "Right:" + counterC + "Wrong:" + counterW;
+
+    console.log("right!" + " " + counterC); 
+
+
+  } function wrongAnswer2() {
+
     let q2 = document.getElementById("container2");
     let q3 = document.getElementById("container3");
 
@@ -153,6 +155,307 @@ let counterW = 0;
     console.log("wrong" + " " + counterW); 
   };
 
+  //question 3
+  function rightAnswer3() {
+
+    let q3 = document.getElementById("container3");
+    let q4 = document.getElementById("container4");
+
+    if (q3.style.display === "block") {
+        q3.style.display = "none"      
+    } 
+    if (q4.style.display === "none") {
+        q4.style.display = "block"
+    }
+    counterC++;
+
+    time++;time++;time++;time++;time++;
+    time++;time++;time++;time++;time++;
+   
+    const scr = document.getElementById("score").innerHTML =
+    "Right:" + counterC + "Wrong:" + counterW;
+
+    console.log("right!" + " " + counterC); 
+
+
+  } function wrongAnswer3() {
+
+    let q3 = document.getElementById("container3");
+    let q4 = document.getElementById("container4");
+
+    if (q3.style.display === "block") {
+        q3.style.display = "none"      
+    } 
+    if (q4.style.display === "none") {
+        q4.style.display = "block"
+    }
+    counterW++;
+
+    time--;time--;time--;time--;time--;
+    time--;time--;time--;time--;time--;
+    time--;time--;time--;time--;time--;
+    time--;time--;time--;time--;time--;
+    time--;time--;time--;time--;time--;
+    time--;time--;time--;time--;time--;
+
+    const wg = document.getElementById("score").innerHTML =
+    "Right:" + counterC + "Wrong:" + counterW;
+
+    console.log("wrong" + " " + counterW); 
+  };
+ 
+  //question 4
+  function rightAnswer4() {
+
+    let q4 = document.getElementById("container4");
+    let q5 = document.getElementById("container5");
+
+    if (q4.style.display === "block") {
+        q4.style.display = "none"      
+    } 
+    if (q5.style.display === "none") {
+        q5.style.display = "block"
+    }
+    counterC++;
+
+    time++;time++;time++;time++;time++;
+    time++;time++;time++;time++;time++;
+   
+    const scr = document.getElementById("score").innerHTML =
+    "Right:" + counterC + "Wrong:" + counterW;
+
+    console.log("right!" + " " + counterC); 
+
+
+  } function wrongAnswer4() {
+
+    let q4 = document.getElementById("container4");
+    let q5 = document.getElementById("container5");
+
+    if (q4.style.display === "block") {
+        q4.style.display = "none"      
+    } 
+    if (q5.style.display === "none") {
+        q5.style.display = "block"
+    }
+    counterW++;
+
+    time--;time--;time--;time--;time--;
+    time--;time--;time--;time--;time--;
+    time--;time--;time--;time--;time--;
+    time--;time--;time--;time--;time--;
+    time--;time--;time--;time--;time--;
+    time--;time--;time--;time--;time--;
+
+    const wg = document.getElementById("score").innerHTML =
+    "Right:" + counterC + "Wrong:" + counterW;
+
+    console.log("wrong" + " " + counterW); 
+  };
+ 
+  //question 5   
+  function rightAnswer5() {
+
+    let q5 = document.getElementById("container5");
+    let q6 = document.getElementById("container6");
+
+    if (q5.style.display === "block") {
+        q5.style.display = "none"      
+    } 
+    if (q6.style.display === "none") {
+        q6.style.display = "block"
+    }
+    counterC++;
+
+    time++;time++;time++;time++;time++;
+    time++;time++;time++;time++;time++;
+   
+    const scr = document.getElementById("score").innerHTML =
+    "Right:" + counterC + "Wrong:" + counterW;
+
+    console.log("right!" + " " + counterC); 
+
+
+  } function wrongAnswer5() {
+
+    let q5 = document.getElementById("container5");
+    let q6 = document.getElementById("container6");
+
+    if (q5.style.display === "block") {
+        q5.style.display = "none"      
+    } 
+    if (q6.style.display === "none") {
+        q6.style.display = "block"
+    }
+    counterW++;
+
+    time--;time--;time--;time--;time--;
+    time--;time--;time--;time--;time--;
+    time--;time--;time--;time--;time--;
+    time--;time--;time--;time--;time--;
+    time--;time--;time--;time--;time--;
+    time--;time--;time--;time--;time--;
+
+    const wg = document.getElementById("score").innerHTML =
+    "Right:" + counterC + "Wrong:" + counterW;
+
+    console.log("wrong" + " " + counterW); 
+  };
+
+  //question 6   
+  function rightAnswer6() {
+
+    let q6 = document.getElementById("container6");
+    let q7 = document.getElementById("container7");
+
+    if (q6.style.display === "block") {
+        q6.style.display = "none"      
+    } 
+    if (q7.style.display === "none") {
+        q7.style.display = "block"
+    }
+    counterC++;
+
+    time++;time++;time++;time++;time++;
+    time++;time++;time++;time++;time++;
+   
+    const scr = document.getElementById("score").innerHTML =
+    "Right:" + counterC + "Wrong:" + counterW;
+
+    console.log("right!" + " " + counterC); 
+
+
+  } function wrongAnswer6() {
+
+    let q6 = document.getElementById("container6");
+    let q7 = document.getElementById("container7");
+
+    if (q6.style.display === "block") {
+        q6.style.display = "none"      
+    } 
+    if (q7.style.display === "none") {
+        q7.style.display = "block"
+    }
+    counterW++;
+
+    time--;time--;time--;time--;time--;
+    time--;time--;time--;time--;time--;
+    time--;time--;time--;time--;time--;
+    time--;time--;time--;time--;time--;
+    time--;time--;time--;time--;time--;
+    time--;time--;time--;time--;time--;
+
+    const wg = document.getElementById("score").innerHTML =
+    "Right:" + counterC + "Wrong:" + counterW;
+
+    console.log("wrong" + " " + counterW); 
+  };
+
+  //question 7   
+  function rightAnswer7() {
+
+    let q7 = document.getElementById("container7");
+    let q8 = document.getElementById("container8");
+
+    if (q7.style.display === "block") {
+        q7.style.display = "none"      
+    } 
+    if (q8.style.display === "none") {
+        q8.style.display = "block"
+    }
+    counterC++;
+
+    time++;time++;time++;time++;time++;
+    time++;time++;time++;time++;time++;
+   
+    const scr = document.getElementById("score").innerHTML =
+    "Right:" + counterC + "Wrong:" + counterW;
+
+    console.log("right!" + " " + counterC); 
+
+
+  } function wrongAnswer7() {
+
+    let q7 = document.getElementById("container7");
+    let q8 = document.getElementById("container8");
+
+    if (q7.style.display === "block") {
+        q7.style.display = "none"      
+    } 
+    if (q8.style.display === "none") {
+        q8.style.display = "block"
+    }
+    counterW++;
+
+    time--;time--;time--;time--;time--;
+    time--;time--;time--;time--;time--;
+    time--;time--;time--;time--;time--;
+    time--;time--;time--;time--;time--;
+    time--;time--;time--;time--;time--;
+    time--;time--;time--;time--;time--;
+
+    const wg = document.getElementById("score").innerHTML =
+    "Right:" + counterC + "Wrong:" + counterW;
+
+    console.log("wrong" + " " + counterW); 
+  };
+
+  //question 8 (final question)   
+  function rightAnswer8() {
+
+    let q8 = document.getElementById("container8");
+    const st = document.getElementById("Start");
+
+    if (q8.style.display === "block") {
+        q8.style.display = "none"      
+    } 
+    if (st.style.display === "none") {
+        st.style.display = "block"
+    }
+    counterC++;
+
+    time++;time++;time++;time++;time++;
+    time++;time++;time++;time++;time++;
+   
+    const scr = document.getElementById("score").innerHTML =
+    "Right:" + counterC + " " + "Wrong:" + counterW;
+
+    console.log("right!" + " " + counterC); 
+    console.log( info, score, time, "Good Job!" );
+    
+   clearInterval(timeLeft);
+
+  } function wrongAnswer8() {
+
+    let q8 = document.getElementById("container8");
+    const st = document.getElementById("Start");
+
+
+    if (q8.style.display === "block") {
+        q8.style.display = "none"      
+    }
+     if (st.style.display === "none") {
+          st.style.display = "block"
+    }
+    counterW++;
+
+    time--;time--;time--;time--;time--;
+    time--;time--;time--;time--;time--;
+    time--;time--;time--;time--;time--;
+    time--;time--;time--;time--;time--;
+    time--;time--;time--;time--;time--;
+    time--;time--;time--;time--;time--;
+
+    const wg = document.getElementById("score").innerHTML =
+    "Right:" + counterC + " " + "Wrong:" + counterW;
+
+    console.log("wrong" + " " + counterW); 
+    console.log( info, score, time, "Good Job!" );
+   
+    clearInterval(timeLeft);
+  };
+
+     
      
   
 
